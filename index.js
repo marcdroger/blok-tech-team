dcc64393d55d36b1447103897601cffe808ddddc
Marc Droger <marcdroger@hotmail.com>
2022-06-05 17:11:44 +0200

    Setup express, nodemon & pug

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello world')
+require('dotenv').config;
+
+const port = process.env.PORT || 3000;
+
+const express = require('express');
+const app = express();
+
+//use public folder
+app.use(express.static('public'));
+
+//use json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//set templating to pug
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+//listen on port
+app.listen(port, () => {
+  console.log(`express running on port ${port}`);
+})
