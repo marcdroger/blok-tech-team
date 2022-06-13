7cff5187fd47269a369d7c504643e2e89705f796
AllyssaA <a.alimoestar@gmail.com>
2022-06-13 15:29:08 +0200

    routes aangemaakt voor account en overview

--- controller/mainController.js
+++ controller/mainController.js
@@ -8,6 +8,14 @@ router.get('/', (req, res) => {
   res.render('index');
 })
 
+router.get('/account', (req, res) => {
+  res.render('account');
+})
+
+router.get('/overview', (req, res) => {
+  res.render('overview')
+})
+
 //render 404 page
 router.use((req, res) => {
   res.status(404).render('404');
