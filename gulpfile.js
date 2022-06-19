a12ffbd18067a179e8226570f7c743a7d2a1d1e4
Marc Droger <marcdroger@hotmail.com>
2022-06-19 19:58:58 +0200

    Minify css in gulp toegevoegd
    Npm package toegevoegd die css minified.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
 
-// Convert sass to css in public folder
+// Convert sass to minified css in public folder
 gulp.task('styles', () => {
   return gulp.src('./sass/style.scss')
   .pipe(sass().on('error', sass.logError))
+  .pipe(cleanCSS({ compatibility: 'ie8'}))
   .pipe(gulp.dest('./public/css/'));
 })
 
