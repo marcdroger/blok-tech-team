7c19e4358e70679e2d6cd390ed08ea8868b057bd
AllyssaA <a.alimoestar@gmail.com>
2022-06-22 16:42:10 +0200

    code in comments verwijderd

--- routes/update.js
+++ routes/update.js
@@ -4,8 +4,4 @@ async function updateUser(req, res) {
     updateStudent({_id: req.body.id}, req.body) ? res.redirect('/account') : res.redirect('/404') 
 }
 
-// router.post('/update', async (req, res) => {
-//     updateStudent({_id: req.body.id}, req.body) ? res.redirect('/account') : res.redirect('/404')
-//   });
-
 module.exports = updateUser
