b06048c1fdba4a7bce498dac9fabd5e0614c536d
Marc Droger <marcdroger@hotmail.com>
2022-06-22 17:38:56 +0200

    Fix indenting. MaincontrollerJS verwijderd (word niet gebruikt)

--- routes/addUser.js
+++ routes/addUser.js
@@ -4,68 +4,69 @@ const { body, validationResult } = require('express-validator');
 const nodemailer = require('nodemailer');
 const addStudent = require('../controller/modules/addStudent')
 
-
 addUser.get('/add', async(req, res) => {
-    hideNav = true;
-  
-    res.render("add", { hideNav });
-  })
+  //hide nav on this page
+  hideNav = true;
+
+  res.render("add", { hideNav });
+})
 
 addUser.post('/add', async (req, res) => {
-    const errors = validationResult(req)
+  const errors = validationResult(req)
 
-    if(!errors.isEmpty()) {
-      return res.status(400).json({
-          success: false,
-          errors: errors.array()
-      });
-    }
-    const student = {
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      email: req.body.email,
-      education: req.body.education,
-      currentSchool: req.body.school,
-      countryPreference: req.body.country
-    }
+  if(!errors.isEmpty()) {
+    return res.status(400).json({
+        success: false,
+        errors: errors.array()
+    });
+  }
 
-    async function mail() {
-      // create reusable transporter object using the default SMTP transport
-      let transporter = nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
-        port: 587,
-        secure: false,
-        requireTLS:true,
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS,
-        },
-        tls:{
-          ciphers:'SSLv3',
-          rejectUnauthorized:false
-        }
-      });
+  const student = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    education: req.body.education,
+    currentSchool: req.body.school,
+    countryPreference: req.body.country
+  }
 
-      // send mail with defined transport object
-      let info = await transporter.sendMail({
-        from: '"Tech team 3" <nigelfijnheertest@outlook.com>',
-        to: req.body.email,
-        subject: "Welcome",
-        text: "Your account have been created.",
-        html: "<b>Your account details are</b><br>" +
-        "<p>Your name: " + req.body.firstname + " " + req.body.lastname +"</p>" +
-        "<p>Your education: " + req.body.education + "</p>" +
-        "<p>Your school: " + req.body.school + "</p>" +
-        "<p>Your preferred country: " + req.body.country + "</p>",
-      });
+  async function mail() {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: 587,
+      secure: false,
+      requireTLS:true,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+      tls:{
+        ciphers:'SSLv3',
+        rejectUnauthorized:false
+      }
+    });
 
-      console.log("Message sent: %s", info.messageId);
-    }
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Tech team 3" <nigelfijnheertest@outlook.com>',
+      to: req.body.email,
+      subject: "Welcome",
+      text: "Your account have been created.",
+      html: "<b>Your account details are</b><br>" +
+      "<p>Your name: " + req.body.firstname + " " + req.body.lastname +"</p>" +
+      "<p>Your education: " + req.body.education + "</p>" +
+      "<p>Your school: " + req.body.school + "</p>" +
+      "<p>Your preferred country: " + req.body.country + "</p>",
+    });
 
-    mail().catch(console.error);
+    console.log("Message sent: %s", info.messageId);
+  }
 
-    addStudent(student);
-    res.redirect("/");
+  mail().catch(console.error);
+
+  addStudent(student);
+  res.redirect("/");
 })
 
 module.exports = addUser
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ let hideNav;
 let session;
 let userId;
 
+//create session with secret string from .env variable
 index.use(sessions({
     secret: process.env.SESSION_SECRET,
     saveUninitialized: true,
@@ -19,55 +20,42 @@ index.use(sessions({
 
 index.get('/', async (req, res) => {
   const students = await getStudents()
-  
+
+  //hide nav on this page
   hideNav = true;
-  
+
   //if user clicks on logo then destroy session
   req.session.destroy();
-  
+
   try {
     res.render('index', {
       student: students,
       hideNav
     });
     } catch (error) {
-        console.log(`Rendering index page failed ${error}`)
+      console.log(`Rendering index page failed ${error}`)
     }
 })
 
+//get userid from the form and puts userid in the session
 index.post('/userselect', (req, res) => {
     userId = req.body.userid;
-    
+
     if(userId) {
       session = req.session;
       session.userid = userId;
-      console.log(req.session);
-    
+
       res.redirect('/matches');
     } else {
       res.redirect('/');
     }
   });
 
-index.post('/userselect', (req, res) => {
-  userId = req.body.userid;
-  
-  if(userId) {
-    session = req.session;
-    session.userid = userId;
-    console.log(req.session);
-  
-    res.redirect('/matches');
-  } else {
-    res.redirect('/');
-  }
-});
-
+//get account information from user by using the userid stored in session
 index.get('/account', async (req, res) => {
     userId = session.userid;
-  
+
     const student = await searchStudent(userId)
-    console.log(student)
     res.render('account', {
       userData: student
     });
@@ -75,11 +63,13 @@ index.get('/account', async (req, res) => {
 
   index.get('/matches', async (req, res) => {
     userId = session.userid;
-    
+
+    //get all students
     const students = await getStudents()
+
     //get student with id from session
     const mainUser = await searchStudent(userId);
-  
+
     //Create new list which filters out the student by the id from the session.
     const updatedList = students.filter(item => item._id != userId);
     try {
